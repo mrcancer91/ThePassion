3da5fe4c9d24b8faf0e48af41b560fb1a9bc593f
runglado <Honggam13@gmail.com>
2013-07-22 20:01:45 +0700

    Cập nhật code test tự động

--- DataAutoTest/AutoTest.php
+++ DataAutoTest/AutoTest.php
@@ -1,10 +1,58 @@
 ﻿<?php
-	$timeStart = microtime( true );
-	//Face Detect program
+	define( "PATH", "DataAutoTest/" );
+	// Auto get image from folder
+	function getImage( $foldername ){
+		$file = array();
+		$dir = opendir( $foldername );
+		while( $r = readdir( $dir ) {
+			if( eregi( "\.jpg", $r )
+				array_push( $file, $r );
+		}
+		return $file;
+	}
 	//
-	$timeEnd = microtime( true );
+	include("FaceDetector.php");
 	
-	// giá trị được tính bằng số khuôn mặt phát hiện được đúng chia cho tổng số khuôn mặt có trong ảnh (tính theo đơn vị phần trăm)
+	//Test program and save result to file
+	function test( $file )
+	{
+		$xmldoc = new DOMDocument();
+		$xmldoc->load( "ResultFile.xml");
+		$root = $xmldoc->getElementsByTagName( "Result" );
+		
+		foreach( $file as $f)
+		{
+			$detector = new FaceDetector();
+			$image = $detector->scanImage( PATH . $f );
+			$size = getimagesize( PATH . $f );
+			
+			$timeStart = microtime( true );
+			// Face Detect program
+			$faces = $detector->faceDetec();
+			//
+			$timeEnd = microtime( true );
+			
+			$count = 0;
+			foreach( $faces as $face) $count++;
+			
+			// write
+			$e = $xmldoc->createElement( "Image" );
+			$a1 = $xmldoc->createAttribute( 'NameID' );
+			$a1->value = $f;
+			$e->appendChild( $a1 );
+			$a2 = $xmldoc->createAttribute( 'Percent' );
+			$a2->value = percent( $f, $size[0], $size[1], $faces, $count );
+			$e->appendChild( $a2 );
+			$a3 = $xmldoc->createAttribute( 'TimeProcess' );
+			$a3->value = ( $timeEnd - $timeStart)*1000 );
+			$e->appendChild( $a3 ;
+			
+			$r->appendChild( $e );
+			
+			$xmldoc->saveXML();
+		}
+	}
+	// return: The percentage of correct detection
 	function percent($idImage, $widthImage, $heightImage, $listface, $numberface)
 	{
 		$xmlDoc = new DOMDocument();
@@ -19,7 +67,7 @@
 			if( $id == $idImage )
 			{
 				$faces = $image->getElementsByTagName( 'face' );
-				$count = 0; $countTrue = 0;
+				$count = 0; $countTrue = 0.0;
 				foreach( $faces as $face )
 				{
 					$xData = $face->getElementsByTagName( 'x' )->item(0)->nodeValue;
@@ -34,13 +82,18 @@
 						$y = $fa['y'];
 						$width = $fa['width'];
 						$height = $fa['height'];
-						$pixelW = $widthImage * 5 / 100;
-						$pixelH = $heightImage * 5 / 100;
 						
 						//Compare face position
-						if( abs( $x - $xData ) <= $pixelW && abs( $y - $yData ) <= $pixelH)
-							if( abs( $width - $widthData ) <= $pixelW && abs( $height - $heightData ) <= $pixelH)
-								$countTrue++;
+						if( abs( $x - $xData ) <= $widthImage * 5 / 100 && abs( $y - $yData ) <= $heightImage * 5 / 100 && abs( $width - $widthData ) <= $widthImage * 5 / 100 && abs( $height - $heightData ) <= $heightImage * 5 / 100)
+							$countTrue++;
+						else if( abs( $x - $xData ) <= $widthImage * 7 / 100 && abs( $y - $yData ) <= $heightImage * 7 / 100 && abs( $width - $widthData ) <= $widthImage * 7 / 100 && abs( $height - $heightData ) <= $heightImage * 7 / 100)
+							$countTrue += 0.9;
+						else if( abs( $x - $xData ) <= $widthImage * 10 / 100 && abs( $y - $yData ) <= $heightImage * 10 / 100 && abs( $width - $widthData ) <= $widthImage * 10 / 100 && abs( $height - $heightData ) <= $heightImage * 10 / 100)
+							$countTrue += 0.8;
+						else if( abs( $x - $xData ) <= $widthImage * 12 / 100 && abs( $y - $yData ) <= $heightImage * 12 / 100 && abs( $width - $widthData ) <= $widthImage * 12 / 100 && abs( $height - $heightData ) <= $heightImage * 12 / 100)
+							$countTrue += 0.7;
+						else if( abs( $x - $xData ) <= $widthImage * 15 / 100 && abs( $y - $yData ) <= $heightImage * 15 / 100 && abs( $width - $widthData ) <= $widthImage * 15 / 100 && abs( $height - $heightData ) <= $heightImage * 15 / 100)
+							$countTrue += 0.6;
 					}
 					$count++;
 				}
@@ -50,15 +103,5 @@
 		$per = ($countTrue / $count * 100);
 		return $per;
 	}
-	
-	//$face['x'] = 200;
-	//$face['y'] = 140;
-	//$face['width'] = 160;
-	//$face['height'] = 167;
-	//$list[0] = $face;
-	
-	echo "Độ chính xác: " . percent( , , , , );
-	
-	// Processing time is calculated by milliseconds
-	echo "<br />Processing time: " . ($timeEnd - $timeStart)*1000;
+
 ?>
